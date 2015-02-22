3756e272767303b8249540ef3ca20644b61462d1
Somrlik <somrlik@gmail.com>
2015-02-22 22:59:50 +0100

    Project structure

---
+++ src/main/java/cz/cvut/fel/syrovkar/studentRPG/StudentQuest.java
@@ -0,0 +1,15 @@
+package cz.cvut.fel.syrovkar.studentRPG;
+
+/**
+ * Created by Karel on 19. 2. 2015.
+ */
+public class StudentQuest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Blgh");
+
+
+    }
+}
---
+++ src/test/java/cz/cvut/fel/syrovkar/studentRPG/tests/firstTest.java
@@ -0,0 +1,8 @@
+package cz.cvut.fel.syrovkar.studentRPG.tests;
+
+/**
+ * Created by Karel on 19. 2. 2015.
+ */
+public class firstTest {
+
+}
